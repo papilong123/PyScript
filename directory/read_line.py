011f0e5049310f30507d124b6cec5191fcacac41
a <2502643682@qq.com>
2021-05-12 10:05:06 +0800

    添加read_list函数

---
+++ directory/read_line.py
@@ -0,0 +1,9 @@
+
+def read_list(list_file):
+    f = open(list_file, "r")
+    lines = f.readlines()
+    list_sig = []
+    for x in lines:
+        list_sig.append(x.rstrip())
+    f.close()
+    return list_sig
